1f46af84ad2bdc434621f4217410235ee90a8d1a
Eloy Llacer <eloy4t@gmail.com>
2017-07-06 17:35:02 +0200

    Temporizador y Modificar tiempo temporizador desde configuracion

--- app/src/main/java/es/elb4t/pictionary/ActividadPrincipal.java
+++ app/src/main/java/es/elb4t/pictionary/ActividadPrincipal.java
@@ -1,7 +1,10 @@
 package es.elb4t.pictionary;
 
+import android.app.Dialog;
 import android.app.Presentation;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,18 +12,25 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.hardware.display.DisplayManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,7 +39,7 @@ import java.util.List;
 public class ActividadPrincipal extends AppCompatActivity {
     List<String> palabras = new ArrayList<String>();
     Button btnComenzar;
-    TextView txtPalabra;
+    TextView txtPalabra, txtLocalTimer, txtRemoteTimer;
     DrawingView dvRemote;
     DrawingView dvLocal;
     Display localDisplay;
@@ -40,14 +50,17 @@ public class ActividadPrincipal extends AppCompatActivity {
     Integer altoRemote = 0;
     Float proporcionAncho = 1.0f;
     Float proporcionAlto = 1.0f;
-    Presentation remotePresentation;
-    Presentation localPresentation;
+    RemotePresentation remotePresentation;
+    RemotePresentation localPresentation;
     private Paint mPaint;
     private DisplayManager mDisplayManager;
+    MiCountDownTimer timer = null;
+     RelativeLayout root;
+    int tiempoDibujar = 60;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         palabras.add("Refugio antiaéreo");
@@ -60,13 +73,13 @@ public class ActividadPrincipal extends AppCompatActivity {
         palabras.add("Abuelo");
         palabras.add("Galaxia");
 
+        tiempoDibujar = obtenerPreferencias();
         txtPalabra = (TextView) findViewById(R.id.txtPalabra);
         btnComenzar = (Button) findViewById(R.id.btnComenzar);
         btnComenzar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mDisplayManager = (DisplayManager)
                         getSystemService(Context.DISPLAY_SERVICE);
-
                 Display[] displays;
                 displays = mDisplayManager.getDisplays();
                 int width;
@@ -92,32 +105,92 @@ public class ActividadPrincipal extends AppCompatActivity {
                     proporcionAncho = anchoRemote / aux;
                     aux = altoLocal / 1.0f;
                     proporcionAlto = altoRemote / aux;
-                    String palabra = "";
-                    Double aleatorio;
-                    aleatorio = Math.random() * (palabras.size() - 1);
-                    Integer numero;
-                    numero = aleatorio.intValue();
-                    palabra = palabras.get(numero);
-                    txtPalabra.setText(palabra);
+
                     Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
                         public void run() {
+                            mostrarPalabraADibujar();
+                        }
+                    }, 500);
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
                             showLocalPresentation();
+                            timer = new MiCountDownTimer(tiempoDibujar * 1000,1000);
+                            timer.start();
                         }
                     }, 5000);
+
                 }
             }
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.cuenta_atras) {
+            final Dialog dialog = new Dialog(this, R.style.Theme_AppCompat_Light_Dialog_Alert);
+            dialog.setContentView(R.layout.conf_tiempo_dibujar);
+            dialog.setTitle("Tiempo cuenta atrás para dibujar");
+            dialog.setCancelable(false);
+
+
+            final EditText ed = (EditText)dialog.findViewById(R.id.editTiempoDibujar);
+            ed.setText(tiempoDibujar+"");
+
+
+            Button bGuardar =(Button)dialog.findViewById(R.id.bGuardar);
+            bGuardar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tiempoDibujar = Integer.parseInt(ed.getText().toString());
+                    guardarPreferencias(tiempoDibujar);
+
+                    Log.e("DIALOG","OK-----");
+                    dialog.hide();
+                }
+            });
+            Button bCancelar =(Button)dialog.findViewById(R.id.bCancelar);
+            bCancelar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("DIALOG","cancel-----");
+                    dialog.cancel();
+                }
+            });
+            dialog.show();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
     private void showRemotePresentation() {
-        RemotePresentation remotePresentation = new RemotePresentation(this, remoteDisplay);
+        remotePresentation = new RemotePresentation(this, remoteDisplay);
         remotePresentation.local = false;
         remotePresentation.show();
     }
 
+    private void mostrarPalabraADibujar() {
+        String palabra = "";
+        Double aleatorio;
+        aleatorio = Math.random() * (palabras.size() - 1);
+        Integer numero;
+        numero = aleatorio.intValue();
+        palabra = palabras.get(numero);
+        txtPalabra.setText(palabra);
+    }
+
     private void showLocalPresentation() {
-        RemotePresentation localPresentation =
+        localPresentation =
                 new RemotePresentation(this, localDisplay);
         localPresentation.local = true;
         localPresentation.show();
@@ -149,13 +222,24 @@ public class ActividadPrincipal extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.remoto);
+            root = (RelativeLayout) findViewById(R.id.pantallaDibbujar);
             if (local == false) {
                 dvRemote = new DrawingView(getApplicationContext());
-                setContentView(dvRemote);
+                root.addView(dvRemote);
+                txtRemoteTimer = new TextView(getApplicationContext());
+                txtRemoteTimer.setLayoutParams(new FrameLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+                txtRemoteTimer.setGravity(Gravity.CENTER_HORIZONTAL);
+                txtRemoteTimer.setTextSize(25);
+                root.addView(txtRemoteTimer);
             } else {
                 dvLocal = new DrawingView(getApplicationContext());
                 dvLocal.setVistaLocal(local);
-                setContentView(dvLocal);
+                root.addView(dvLocal);
+                txtLocalTimer = new TextView(getApplicationContext());
+                txtLocalTimer.setLayoutParams(new FrameLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+                txtLocalTimer.setGravity(Gravity.CENTER_HORIZONTAL);
+                txtRemoteTimer.setTextSize(25);
+                root.addView(txtLocalTimer);
             }
             mPaint = new Paint();
             mPaint.setAntiAlias(true);
@@ -165,6 +249,14 @@ public class ActividadPrincipal extends AppCompatActivity {
             mPaint.setStrokeJoin(Paint.Join.ROUND);
             mPaint.setStrokeCap(Paint.Cap.ROUND);
             mPaint.setStrokeWidth(12);
+        }
+
+        @Override
+        public void onBackPressed() {
+            //super.onBackPressed();
+            timer.cancel();
+            timer.onFinish();
+            Log.e("BBACK","Back pressed-----");
         }
     }
 
@@ -278,5 +370,40 @@ public class ActividadPrincipal extends AppCompatActivity {
             }
             return true;
         }
+    }
+
+    public class MiCountDownTimer extends CountDownTimer {
+
+        public MiCountDownTimer(long starTime, long interval) {
+            super(starTime, interval);
+
+        }
+
+        @Override
+        public void onFinish() {
+            hideLocalPresentation(localDisplay);
+            hideRemotePresentation(remoteDisplay);
+            Log.e("TIME","onFINISH---------");
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            txtLocalTimer.setText(millisUntilFinished/1000+"");
+            txtRemoteTimer.setText(millisUntilFinished/1000+"");
+        }
+
+    }
+
+    public int obtenerPreferencias() {
+        SharedPreferences prefs = getSharedPreferences("pictionary", Context.MODE_PRIVATE);
+        tiempoDibujar = prefs.getInt("tiempoDibujar", 60);
+        return tiempoDibujar;
+    }
+
+    public void guardarPreferencias(int tiempo) {
+        SharedPreferences prefs = getSharedPreferences("pictionary", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("tiempoDibujar", tiempo);
+        editor.commit();
     }
 }
